4ee0fd359ecd2f48c1dfe813f0feb55a3a19ff72
Doug Binks <doug.binks@gmail.com>
2013-01-22 16:47:30 +0100

    Removal of unrequired OpenAL and Audio code.

--- Aurora/External/openal/linux/src/al_propagation.h
+++
@@ -1,8 +0,0 @@
-#ifndef AL_PROPAGATION_H_
-#define AL_PROPAGATION_H_
-
-#include <AL/altypes.h>
-
-void _alPropagationSpeed(ALfloat value);
-
-#endif /* AL_DISTANCE_H_ */
--- Aurora/External/openal/linux/src/arch/solaris/README
+++
@@ -1,1 +0,0 @@
-Not implemented yet
