aaac0ada7c8081da13f59dbae4680eca9d763999
Doug Binks <doug@enkisoftware.com>
2014-08-23 16:05:58 +0100

    Changed to have IRuntimeObjectSystem derive from ITestBuildNotifier for ease of use.

--- Aurora/RuntimeObjectSystem/IRuntimeObjectSystem.h
+++ Aurora/RuntimeObjectSystem/IRuntimeObjectSystem.h
@@ -62,7 +62,7 @@ namespace FileSystemUtils
     class Path;
 }
 
-struct IRuntimeObjectSystem
+struct IRuntimeObjectSystem : public ITestBuildNotifier
 {
 	// Initialise RuntimeObjectSystem. pLogger and pSystemTable should be deleted by creator. 
 	// Both pLogger and pSystemTable can be 0
