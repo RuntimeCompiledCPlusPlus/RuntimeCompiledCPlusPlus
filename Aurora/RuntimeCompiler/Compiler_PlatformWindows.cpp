a112af04fe4d0754bc59b77de5f360caf4dfaec9
Doug Binks <doug@enkisoftware.com>
2018-05-10 12:38:12 +0100

    Support for Visual Studio 15.7

--- Aurora/RuntimeCompiler/Compiler_PlatformWindows.cpp
+++ Aurora/RuntimeCompiler/Compiler_PlatformWindows.cpp
@@ -466,6 +466,7 @@ void GetPathsOfVisualStudioInstalls( std::vector<VSVersionInfo>* pVersions, ICom
 	case 1911:	//VS 2017
 	case 1912:	//VS 2017
 	case 1913:	//VS 2017
+	case 1914:	//VS 2017
 		startVersion = 6;
 		break;
 	default:
