5cefb6c356ab608c16ade34d0d95377069c8696c
GBazior <bazior@agh.edu.pl>
2025-10-29 22:39:36 +0100

    Make RuntimeObject01 more modern C++ (use override keyword)

--- Aurora/Examples/ConsoleExample/RuntimeObject01.cpp
+++ Aurora/Examples/ConsoleExample/RuntimeObject01.cpp
@@ -1,18 +1,17 @@
 #include "../../RuntimeObjectSystem/ObjectInterfacePerModule.h"
-
 #include "../../RuntimeObjectSystem/IObject.h"
 #include "IUpdateable.h"
 #include "InterfaceIds.h"
 #include <iostream>
 
 
-class RuntimeObject01 : public TInterface<IID_IUPDATEABLE,IUpdateable>
+class RuntimeObject01 : public TInterface<IID_IUPDATEABLE, IUpdateable>
 {
 public:
-	virtual void Update( float deltaTime )
-	{
-		std::cout << "Runtime Object 01 update called!\n";
-	}
+    virtual void Update( float deltaTime ) override
+    {
+        std::cout << "Runtime Object 01 update called!" << std::endl;
+    }
 };
 
 REGISTERCLASS(RuntimeObject01);
