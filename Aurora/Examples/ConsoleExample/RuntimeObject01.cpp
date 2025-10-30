7886cf944abd42ffda659b8024cb19af9c8be5bb
GBazior <bazior@agh.edu.pl>
2025-10-30 14:07:00 +0100

    Revert "Make RuntimeObject01 more modern C++ (use override keyword)"
    This reverts commit 5cefb6c356ab608c16ade34d0d95377069c8696c.

--- Aurora/Examples/ConsoleExample/RuntimeObject01.cpp
+++ Aurora/Examples/ConsoleExample/RuntimeObject01.cpp
@@ -1,17 +1,18 @@
 #include "../../RuntimeObjectSystem/ObjectInterfacePerModule.h"
+
 #include "../../RuntimeObjectSystem/IObject.h"
 #include "IUpdateable.h"
 #include "InterfaceIds.h"
 #include <iostream>
 
 
-class RuntimeObject01 : public TInterface<IID_IUPDATEABLE, IUpdateable>
+class RuntimeObject01 : public TInterface<IID_IUPDATEABLE,IUpdateable>
 {
 public:
-    virtual void Update( float deltaTime ) override
-    {
-        std::cout << "Runtime Object 01 update called!" << std::endl;
-    }
+	virtual void Update( float deltaTime )
+	{
+		std::cout << "Runtime Object 01 update called!\n";
+	}
 };
 
 REGISTERCLASS(RuntimeObject01);
