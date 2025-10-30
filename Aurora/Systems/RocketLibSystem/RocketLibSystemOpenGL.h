369e7db843aa6e32eb8d310a76aae848ba03eafc
Doug Binks <doug@enkisoftware.com>
2025-10-30 19:09:17 +0000

    Removed glu.h use

--- Aurora/Systems/RocketLibSystem/RocketLibSystemOpenGL.h
+++ Aurora/Systems/RocketLibSystem/RocketLibSystemOpenGL.h
@@ -34,15 +34,12 @@
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
 #include <gl/Gl.h>
-#include <gl/Glu.h>
 #elif defined ROCKET_PLATFORM_MACOSX
 #include <OpenGL/gl.h>
-#include <OpenGL/glu.h>
 #include <OpenGL/glext.h>
 #elif defined ROCKET_PLATFORM_UNIX
 #include <GL/gl.h>
 #include <GL/glext.h>
-#include <GL/glu.h>
 #endif
 
 #endif
